7a77f2d3925eff7925a4109493d1a584f49d20f5
= <=>
2025-08-12 10:05:00 +0700

    reset

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
@@ -10,5 +11,4 @@ async function bootstrap() {
   });
   await app.listen(process.env.PORT ?? 3001);
 }
-
 bootstrap();
